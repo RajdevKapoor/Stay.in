4c1cb4bfbd94c7938e573e3a39b9755922e886ff
dkanchanapalli <94420864+dkanchanapalli@users.noreply.github.com>
2022-02-04 14:57:40 -0500

    Initial backend logic

---
+++ stay-in/backend/routes/routes.go
@@ -0,0 +1,14 @@
+package routes
+
+import (
+	"../controllers"
+	"github.com/gofiber/fiber"
+)
+
+func Setup(app *fiber.App) {
+
+	app.Post("/api/register", controllers.Register)
+	app.Post("/api/login", controllers.Login)
+	app.Get("/api/user", controllers.User)
+
+}
