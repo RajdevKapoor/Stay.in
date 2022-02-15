955c9e53538c7c13490638af510af0e241bbbd08
dkanchanapalli <94420864+dkanchanapalli@users.noreply.github.com>
2022-02-15 14:47:01 -0500

    auth modifications

--- stay-in/backend/controllers/auth.go
+++ stay-in/backend/controllers/auth.go
@@ -3,27 +3,33 @@ package controllers
 import (
 	"fmt"
 
-	"../dbase"
-	"../model"
+	"gotest/dbase"
+	"gotest/model"
 	"github.com/gofiber/fiber"
 )
 
+
+
 func Signup(c *fiber.Ctx) {
 	db := dbase.DBConn
+	fmt.Println(db)
 	var u model.User
 	u.FirstName = "Dharani"
 	u.LastName = "Kanchanapalli"
 	u.Email = "dharanik@gmail.com"
 	u.Password = "1234"
-	fmt.Println(db.Create(&u))
+	db.Create(&u)
+	fmt.Println("signup working")
+	// fmt.Println(db.Create(&u))
 	fmt.Println(c.JSON(u))
 }
 
 func Signin(c *fiber.Ctx) {
-	email := c.Params("email")
+	// email := c.Params("email")
 	db := dbase.DBConn
-	var u model.User
-	db.Find(&u, email)
+	var u []model.User
+	db.Raw("select * from users").Scan(&u)
+	fmt.Println(u)
 	fmt.Println("user successfully signed in")
 	c.JSON(u)
 }
