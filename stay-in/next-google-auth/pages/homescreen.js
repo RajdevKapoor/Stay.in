fcf4c2d184423d3b93a06f16d50f201aa071efe1
Rajdev Kapoor <rajdev1409@gmail.com>
2022-03-03 15:41:04 -0500

    added authenticated routes

---
+++ stay-in/next-google-auth/pages/homescreen.js
@@ -0,0 +1,28 @@
+import { getSession } from 'next-auth/client';
+import styles from '../styles/Home.module.css';
+
+export default function Homescreen({ user }) {
+  return (
+    <div className={styles.container}>
+      <h1>Dashboard(Protected Route)</h1>
+      <p>
+        Welcome to dashboard: <b>{user.name}</b>
+      </p>
+      <p>{user.email}</p>
+    </div>
+  );
+}
+
+export async function getServerSideProps(context) {
+  const session = await getSession(context);
+  if (!session) {
+    context.res.writeHead(302, { Location: '/' });
+    context.res.end();
+    return {};
+  }
+  return {
+    props: {
+      user: session.user,
+    },
+  };
+}
