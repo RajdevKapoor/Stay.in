25fc0404c54ef19971da362eba8520901cc0d810
shivamgupta-7 <47007121+shivamgupta-7@users.noreply.github.com>
2022-03-04 23:22:57 -0500

    unit tests added
    Unit tests added for Functions

--- stay-in/backend/jwt/controllers/authController.go
+++ stay-in/backend/jwt/controllers/authController.go
@@ -9,44 +9,58 @@ import (
 	"strconv"
 	"time"
 )
+
 const SecretKey = "secret"
+
 func Register(c *fiber.Ctx) error {
 	var data map[string]string
+
+	//Unit test for Register
 	if err := c.BodyParser(&data); err != nil {
 		return err
 	}
+
 	password, _ := bcrypt.GenerateFromPassword([]byte(data["password"]), 14)
 	user := models.User{
 		Email:    data["email"],
 		Password: password,
 	}
+
 	database.DB > Create(&user)
 	return c.JSON(user)
 
 }
+
 func Login(c *fiber.Ctx) error {
 	var data map[string]string
+
+	//Unit Test for Login
 	if err := c.BodyParser(&data); err != nil {
 		return err
 	}
+
 	var user models.User
 	database.DB.Where("email=?", data["email"]).First(&user)
+
 	if user.Id==0{
 		c.Status(fiber.StatusNotFound)
 		return c.JSON(fiber.Map{
 			"message":"user not found"
 		})
 	}
+
 	if err := bcrypt.CompareHashAndPassword(user.Password, []byte(data["password"])); err!= nil{
 		c.Status(fiber.StatusBadRequest)
 		return c.JSON(fiber.Map{
 			"message":"incorrect password",
 		})
 	}
+
 	claims := jwt.NewWithClaims(jwt.SigningMethodHS256,jwt.StandardClaims{
 		Issuer:strconv.Itoa(int(user.ID))
 		ExpiresAt:time.Now().Add(time.Hour * 24).Unix(),
 	})
+
 	token, err := claims.SignedString([]byte(SecretKey))
 	if err != nil {
 		c.Status(fiber.StatusInternalServerError)
@@ -54,31 +68,40 @@ func Login(c *fiber.Ctx) error {
 			"message":"could not login",
 		})
 	}
+
 	cookie :=fiber.Cookie{
 		Name: "jwt",
 		Value: token,
 		ExpiresAt:time.Now().Add(time.Hour * 24),
 		HTTPOnly: true, 
 	}
+
 	c.Cookie(&cookie)
 	return c.JSON(fiber.Map{
 		"message":"success",
 	})
 }
 
+
 func User(c *fiber.Ctx) error{
+	
 	cookie := c.Cookies("jwt")
+	
 	token, err := jwt.ParseWithClaims(cookie,&jwt.StandardClaims{},func(token *jwt.Token)(interface{},error){
 		return []byte(SecretKey),nil
 	})
+	
+	//Unit Test to ensure User Authentication
 	if err != nil {
 		c.Status(fiber.StatusUnauthorized)
 		return c.JSON(fiber.Map{
 			"message":"unauthenticated",
 		})
 	}
+
 	claims := token.Claims.(*jwt.StandardClaims)
 	var user models.User
+	
 	database.DB.Where("id=?",claims.Issuer).First(&user)
 	return c.JSON(user)
 }
