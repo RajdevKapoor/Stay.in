6e4c28592ac273e81fef791b7c5f46927e6f4608
shivamgupta-7 <47007121+shivamgupta-7@users.noreply.github.com>
2022-03-04 23:04:39 -0500

    routes setup for jwt auth setup
    routes setup for jwt auth setup for api Setup func

---
+++ stay-in/backend/jwt/routes/routes.go
@@ -0,0 +1,12 @@
+package routes
+
+import (
+	"../controllers"
+	"github.com/gofiber/fiber/v2"
+)
+
+func Setup(app *fiber.App) {
+	app.Post("/api/register", controllers.Register)
+	app.Post("/api/login", controllers.Login)
+	app.Get("/api/user", controllers.User)
+}
