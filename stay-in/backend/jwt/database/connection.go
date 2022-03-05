47da998a640eafdf46dbb8eaa5c279abef103c40
shivamgupta-7 <47007121+shivamgupta-7@users.noreply.github.com>
2022-03-04 23:24:36 -0500

    unit tests added
    unit tests added for database connection

--- stay-in/backend/jwt/database/connection.go
+++ stay-in/backend/jwt/database/connection.go
@@ -10,9 +10,12 @@ var DB *gorm.DB
 
 func Connect() {
 	connection, err := gorm.Open(sqlite.Open("login.db"), &gorm.Config{})
+
+	//Test for Connection to database
 	if err != nil {
 		panic("failed to connect database")
 	}
+
 	DB = connection
 	connection.AutoMigrate(&models.User{})
 }
