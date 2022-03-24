0af40aa272798ef842adf5536250f7ef73afccc6
shivamgupta-7 <47007121+shivamgupta-7@users.noreply.github.com>
2022-03-23 23:04:22 -0400

    Update main.go

--- stay-in/backend/properties/main.go
+++ stay-in/backend/properties/main.go
@@ -9,22 +9,29 @@ import (
 	"github.com/rajdev/stayin/stay"
 )
 
+//Hello World Test function
 func helloWorld(c *fiber.Ctx) {
 	c.Send("Hello, World!")
 }
 
 func initDatabase() {
 	var err error
+
+	//Database Connection
 	database.DBConn, err = gorm.Open("sqlite3", "stays.db")
+
+	//Unit Test for Database Connection
 	if err != nil {
 		panic("failed to connect database")
 	}
+
 	fmt.Println("Connection Opened to Database")
 
 	database.DBConn.AutoMigrate(&stay.Stay{})
 	fmt.Println("Database Migrated")
 }
 
+//Function to Setup Routes for apis
 func setupRoutes(app *fiber.App) {
 	app.Get("/", helloWorld)
 
@@ -34,6 +41,7 @@ func setupRoutes(app *fiber.App) {
 	app.Delete("/api/v1/stay/:id", stay.Deletestay)
 }
 
+//Main Function
 func main() {
 	app := fiber.New()
 	initDatabase()
