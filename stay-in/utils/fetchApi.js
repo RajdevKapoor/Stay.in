906e0004354ca8aa946b321f450f39783e79ec09
Rajdev Kapoor <rajdev1409@gmail.com>
2022-03-31 18:49:15 -0400

    fetch data api fix

--- stay-in/utils/fetchApi.js
+++ stay-in/utils/fetchApi.js
@@ -1,14 +1,14 @@
-import axios from "axios";
+import axios from 'axios';
 
 export const baseUrl = 'https://bayut.p.rapidapi.com';
 
 export const fetchApi = async (url) => {
-    const { data } = await axios.get((url), {
-        headers: {
-            'x-rapidapi-host': 'bayut.p.rapidapi.com',
-            'x-rapidapi-key': 'c64f38dfefmsh69875933d1b9caep14e3ddjsn2adf8806a2a5'
-        }
-    });
-        
-    return data;
-}
+	const { data } = await axios.get(url, {
+		headers: {
+			'x-rapidapi-host': 'bayut.p.rapidapi.com',
+			'x-rapidapi-key': 'c64f38dfefmsh69875933d1b9caep14e3ddjsn2adf8806a2a5'
+		}
+	});
+
+	return data;
+};
