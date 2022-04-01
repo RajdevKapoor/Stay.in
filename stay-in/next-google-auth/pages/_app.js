94710626a124f2f96e686e28506e4c55b829578b
Rajdev Kapoor <rajdev1409@gmail.com>
2022-04-01 01:05:25 -0400

    Updated app Provider component

--- stay-in/next-google-auth/pages/_app.js
+++ stay-in/next-google-auth/pages/_app.js
@@ -2,9 +2,9 @@ import { Provider } from 'next-auth/client';
 import '../styles/globals.css';
 
 export default function App({ Component, pageProps }) {
-  return (
-    <Provider session={pageProps.session}>
-      <Component {...pageProps} />
-    </Provider>
-  );
+	return (
+		<Provider session={pageProps.session}>
+			<Component {...pageProps} />
+		</Provider>
+	);
 }
