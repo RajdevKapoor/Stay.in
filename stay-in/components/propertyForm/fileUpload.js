6a254a7848a4fd77994a5a335411fb775877a006
Kshitij Sinha <43610780+sinarest1608@users.noreply.github.com>
2022-04-01 01:01:52 -0400

    Frontend: Updated hard-coded Image list

--- stay-in/components/propertyForm/fileUpload.js
+++ stay-in/components/propertyForm/fileUpload.js
@@ -34,18 +34,7 @@ class PicturesWall extends Component {
         status: "done",
         url: "https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png",
       },
-      {
-        uid: "-3",
-        name: "image.png",
-        status: "done",
-        url: "https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png",
-      },
-      {
-        uid: "-4",
-        name: "image.png",
-        status: "done",
-        url: "https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png",
-      },
+      
       {
         uid: "-xxx",
         percent: 50,
@@ -94,6 +83,7 @@ class PicturesWall extends Component {
           fileList={fileList}
           onPreview={this.handlePreview}
           onChange={this.handleChange}
+          className="Upload-Ant"
         >
           {fileList.length >= 8 ? null : uploadButton}
         </Upload>
@@ -114,7 +104,8 @@ class ImageUpload extends React.Component {
 
     render() {
       return (
-        <div className="imgbb">
+        // <div className="imgbb" style={{display:"grid !important" , gridTemplateColumns: "auto auto auto !important"}}>
+        <div className="imgbb" style={{display:"flex" , flexDirection:"column"}}>
           <PicturesWall />
         </div>
       );
