7ac67cc6ffb64e46af1da89dec0fe752f992644f
Rajdev Kapoor <rajdev1409@gmail.com>
2022-04-07 00:42:09 -0400

    added addProperty Funciton

--- stay-in/backend/controllers/authController.go
+++ stay-in/backend/controllers/authController.go
@@ -27,6 +27,22 @@ func Register(c *fiber.Ctx) error {
 	return c.JSON(user)
 
 }
+
+func addProperty(c *fiber.Ctx) error {
+	var data map[string]string
+	if err := c.BodyParser(&data); err != nil {
+		return err
+	}
+	password, _ := bcrypt.GenerateFromPassword([]byte(data["password"]), 14)
+	user := models.User{
+		ID: 0,
+		Email: data["email"],
+		Password: password,
+	}
+	//database.DB > Create(&user)
+	return c.JSON(user)
+
+}
 func Login(c *fiber.Ctx) error {
 	var data map[string]string
 	if err := c.BodyParser(&data); err != nil {
