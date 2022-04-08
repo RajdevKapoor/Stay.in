55734ee13c67c010cf0521ca8f91a3ba8f2df19c
shivamgupta-7 <47007121+shivamgupta-7@users.noreply.github.com>
2022-04-07 21:00:10 -0400

    chat feature backend fixes #32

---
+++ stay-in/backend/chat/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/pusher/pusher-http-go"
+)
+
+func main() {
+	app := fiber.New()
+	app.Use(cors.New())
+	pusherClient := pusher.Client{
+		AppID:   "1382233",
+		Key:     "6448edf1a6faa7f3aaa6",
+		Secret:  "b692dfb1814d55c47836",
+		Cluster: "us2",
+		Secure:  true,
+	}
+	app.Post("/api/messages", func(c *fiber.Ctx) error {
+
+		var data map[string]string
+		if err := c.BodyParser(&data); err != nil {
+			return err
+		}
+		pusherClient.Trigger("chat", "message", data)
+		return c.JSON([]string{})
+	})
+
+	app.Listen(":8000")
+}
