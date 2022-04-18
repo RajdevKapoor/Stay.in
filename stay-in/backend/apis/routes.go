93613290d3353019d882b821d365e933c63021e5
dkanchanapalli <94420864+dkanchanapalli@users.noreply.github.com>
2022-04-17 23:27:18 -0400

    minor fixes to routes.go

---
+++ stay-in/backend/apis/routes.go
@@ -0,0 +1,32 @@
+package routes
+
+import (
+	"expenseManagement/controllers"
+
+	"github.com/gorilla/mux"
+)
+
+func Setup(r *mux.Router) {
+	r.HandleFunc("/users/{id}", controllers.RetrieveUser).Methods("GET", "OPTIONS")
+	r.HandleFunc("/user/register", controllers.InsertUser).Methods("POST", "OPTIONS")
+	r.HandleFunc("/user/login", controllers.LoginUser).Methods("POST", "OPTIONS")
+	// r.HandleFunc("/user/passwordudpate", controllers.UpdateUserPassword).Methods("POST", "OPTIONS")
+
+	//Expense routes
+	r.HandleFunc("/expense/get", controllers.GetExpense).Methods("GET", "OPTIONS")
+	r.HandleFunc("/expense", controllers.InsertExpense).Methods("POST", "OPTIONS")
+	r.HandleFunc("/expense/update", controllers.UpdateExpense).Methods("PUT", "OPTIONS")
+	r.HandleFunc("/expense/remove", controllers.DeleteExpense).Methods("DELETE", "OPTIONS")
+
+	//Payment routes
+	r.HandleFunc("/expense/insertpayment", controllers.InsertPaymentRecord).Methods("POST", "OPTIONS")
+	r.HandleFunc("/expense/deletepayment", controllers.DeletePayment).Methods("DELETE", "OPTIONS")
+
+	//Income routes
+	r.HandleFunc("/expense/insertincome", controllers.InsertIncome).Methods("POST", "OPTIONS")
+	r.HandleFunc("/expense/updateincome", controllers.UpdateIncome).Methods("DELETE", "OPTIONS")
+
+	//PaymentSplit routes
+	r.HandleFunc("/expense/insertpaymentsplit", controllers.InsertPaymentSplitRecord).Methods("POST", "OPTIONS")
+	r.HandleFunc("/expense/deletepaymentsplit", controllers.DeletePaymentSplit).Methods("DELETE", "OPTIONS")
+}
