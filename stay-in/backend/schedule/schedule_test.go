5ed4d6754bd489abd7cb1c3b00a38338e8d81b09
shivamgupta-7 <47007121+shivamgupta-7@users.noreply.github.com>
2022-04-20 21:17:23 -0400

    unit tests updated

--- stay-in/backend/schedule/schedule_test.go
+++ stay-in/backend/schedule/schedule_test.go
@@ -1,4 +1,4 @@
-package main
+package schedule
 
 import (
 	"log"
@@ -17,9 +17,14 @@ func TestMain(m *testing.M) {
 		log.Fatal(err)
 	}
 
+	if err := a.DB.Migrator().HasTable("user_details"); err != true {
+		log.Fatal(err)
+	}
+
 	code := m.Run()
 
 	a.DB.Migrator().DropTable("client_details")
+	a.DB.Migrator().DropTable("user_details")
 
 	os.Exit(code)
 
@@ -33,4 +38,68 @@ func TestGetNonExistentTrackingID(t *testing.T) {
 	if want, got := http.StatusOK, w.Code; want == got {
 		t.Fatalf("expected a %d, instead got: %d", want, got)
 	}
+
+}
+
+func TestGetExistingTrackingID(t *testing.T) {
+
+	var s ClientDetails
+
+	s.TrackingID = "381539592"
+	a.DB.Save(&s)
+
+	req, _ := http.NewRequest("GET", "http://localhost:8080/getClient?trackingID=381539592", nil)
+
+	w := httptest.NewRecorder()
+	a.r.ServeHTTP(w, req)
+
+	if want, got := http.StatusOK, w.Code; want != got {
+		t.Fatalf("expected a %d, instead got: %d", want, got)
+	}
+}
+
+func TestGetNonExistentUsername(t *testing.T) {
+	req, _ := http.NewRequest("GET", "http://localhost:8080/checkUser?firstname=firstname&password=temp", nil)
+	w := httptest.NewRecorder()
+	a.r.ServeHTTP(w, req)
+
+	if want, got := http.StatusOK, w.Code; want == got {
+		t.Fatalf("expected a %d, instead got: %d", want, got)
+	}
+}
+
+func TestGetExistingUsername(t *testing.T) {
+
+	var u UserDetails
+
+	u.FirstName = "backend"
+	u.Password = "temp"
+	a.DB.Save(&u)
+
+	req, _ := http.NewRequest("GET", "http://localhost:8080/checkUser?firstname=backend&password=temp", nil)
+
+	w := httptest.NewRecorder()
+	a.r.ServeHTTP(w, req)
+
+	if want, got := http.StatusOK, w.Code; want != got {
+		t.Fatalf("expected a %d, instead got: %d", want, got)
+	}
+}
+
+func TestIncorrectPassword(t *testing.T) {
+	var u UserDetails
+
+	u.FirstName = "backend"
+	u.Password = "temp"
+	a.DB.Save(&u)
+
+	req, _ := http.NewRequest("GET", "http://localhost:8080/checkUser?firstname=backend&password=tempo", nil)
+
+	w := httptest.NewRecorder()
+	a.r.ServeHTTP(w, req)
+
+	if want, got := http.StatusBadRequest, w.Code; want != got {
+		t.Fatalf("expected a %d, instead got: %d", want, got)
+	}
+
 }
